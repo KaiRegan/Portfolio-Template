3bdd4a74c20bc31f6d883284a55efaa99da4badf
KaiRegan <105380642+KaiRegan@users.noreply.github.com>
2022-06-03 20:33:02 +0100

    This is template 2.0
    Updated template
    
    Co-Authored-By: Edward Patch <54278802+ShinkuKira21@users.noreply.github.com>

---
+++ Client/src/components/navbar/Navbar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    
+</template>
+
+<style>
+    /* CSS HERE (Mainly for #app) */
+</style>
---
+++ Client/src/main.ts
@@ -0,0 +1,5 @@
+// Vite essentially acts as the loader for the frontend...
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app');
